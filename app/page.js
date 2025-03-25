5b2c48a605365eae179d155320ae432bc98cb46b
JakariaJishan <jakariajishan987@gmail.com>
2025-03-25 12:39:26 +0600

    fix

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default async function Home() {
     return (
         <>
             <ShowcaseSection/>
-            <MedicalCondition/>
+            {/*<MedicalCondition/>*/}
             <DividerLine/>
             <WhyChooseUsSection/>
             <AboutUsSection/>
