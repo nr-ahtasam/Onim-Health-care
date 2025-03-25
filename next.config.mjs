eba80ae3d373765d9dc3f983535a66affee2ce0f
JakariaJishan <jakariajishan987@gmail.com>
2025-03-25 13:58:16 +0600

    fix prerendering issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['images.pexels.com','omni.fmmethod.online'], // Add the hostname here
     },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
 };
 
 export default nextConfig;
