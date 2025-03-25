f0815df5c07b525be38e1698b5705b77e9a85c32
JakariaJishan <jakariajishan987@gmail.com>
2025-03-25 12:37:58 +0600

    fix

--- app/page.js
+++ app/page.js
@@ -3,19 +3,15 @@ import MedicalCondition from "@/components/shared/MedicalCondition";
 import WhyChooseUsSection from "@/components/shared/WhyChooseUs";
 import AboutUsSection from "@/components/shared/AboutUs";
 import DividerLine from "@/components/shared/DividerLine";
-import {Suspense} from "react";
-import Loader from "@/lib/Loader";
 
 export default async function Home() {
-  return (
-      <>
-        <ShowcaseSection />
-          <Suspense fallback={<Loader/>}>
+    return (
+        <>
+            <ShowcaseSection/>
             <MedicalCondition/>
-          </Suspense>
-        <DividerLine/>
-        <WhyChooseUsSection />
-        <AboutUsSection />
-      </>
-  );
+            <DividerLine/>
+            <WhyChooseUsSection/>
+            <AboutUsSection/>
+        </>
+    );
 }
