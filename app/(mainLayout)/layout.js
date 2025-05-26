62690d71016b712fcfa821caa8e62b9f8831abb5
Kamrul Hassan <kh27.abir@gmail.com>
2025-05-26 17:37:41 +0600

    rename layouts

--- app/(mainLayout)/layout.js
+++ app/(mainLayout)/layout.js
@@ -1,4 +1,4 @@
-import Footer from "@/app/(site)/footer";
+import Footer from "@/app/(mainLayout)/footer";
 import ApolloWrapper from "@/components/apollo/ApolloWrapper";
 import Header from "@/components/header/Header";
 import BlogCardPreview from "@/components/news-letter/BlogCardPreview";
